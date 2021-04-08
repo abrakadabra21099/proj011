0cb9ab1bc351125d60618dd873ffd237d779ba0e
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-08 11:12:50 +0400

    Learn 33. Done.

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//touchleave
+//touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        // console.log(e.touches);
+        // console.log(e.targetTouches);
+        console.log(e.changedTouches);
+    });
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log(e.targetTouches[0].pageX);
+    });
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('touchend');
+    });
+});
+
+// touches
